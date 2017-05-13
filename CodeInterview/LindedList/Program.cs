a9428c47ae7f457f5cced5f9e36146e750c7beb8
uwspstar <uwspstar@gmail.com>
2017-05-13 08:47:21 -0500

    Linked List code

--- CodeInterview/LindedList/Program.cs
+++ CodeInterview/LindedList/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 
 namespace LindedList
-{
+{   //Node
+    //Node Chain
+    //Linked List
+    //Doubly Linked List
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Node first = new Node { Value = 3 };
+            Node middel = new Node { Value = 5 };
+            Node last = new Node { Value = 7 };
+
+            first.Next = middel;
+            middel.Next = last;
+
+            PrintNodeList(first);
+
+            Console.ReadLine();
         }
+
+        private static void PrintNodeList(Node node)
+        {
+            while (node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
+        }
+    }
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
     }
 }
