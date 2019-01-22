8ed94610f26466ed89d5e6a3bb857a879f8cf402
uwspstar <uwspstar@gmail.com>
2019-01-21 19:03:22 -0600

    no message

--- DataStructures _Algorithms/find-letter-repeat-times.js
+++ DataStructures _Algorithms/find-letter-repeat-times.js
@@ -1,9 +1,25 @@
-//find-letter-repeat-times.js
-
 //find letter repeat times
 //const str ="do not push you luck dude";
 // letterCounter("do not push you luck dude") //d:3, O:3, u:3...
  
+ //solution 2
+ const letterCounter2 = function (str) {
+    let result= {};
+    for(let i=0; i<str.length; i++) {  
+      const char= str[i].toLowerCase();
+      if(result[char] > 0) {
+        result[char] = result[char]+1;
+      } else {
+        result[char] =1;  
+      }
+    } 
+
+    return result;
+ };
+
+ console.log(letterCounter2("do not push you luck dude"));
+
+ //solution 1
  const letterCounter = function (str) {
    
    var map = {} ;
