37a43a1012a710e938afd58107f67a3e7739cc50
xing wang <uwspstar@gmail.com>
2019-02-06 06:30:44 -0600

    Create best-time-to-buy-and-sell-stock.js

---
+++ DataStructures _Algorithms/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/solution/
+//We need to find out the maximum difference 
+//(which will be the maximum profit) 
+//between two numbers in the given array. 
+//Also, the second number (selling price) 
+//must be larger than the first one (buying price).
+
+//In formal terms, we need to 
+//find max(prices[j] - prices[i])
+//max(prices[j]−prices[i]), 
+//for every i and j such that j > i
+
