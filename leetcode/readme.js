6c13249a36c8486c564602a7941471d257b01ed9
xing wang <uwspstar@gmail.com>
2019-05-04 08:48:34 -0500

    Update readme.js

--- leetcode/readme.js
+++ leetcode/readme.js
@@ -1,1 +1,15 @@
 https://leetcode.com/
+
+
+
+## PATTERNS...
+
+    - Frequency Counter
+    - Multiple Pointers
+    - Sliding Window
+    - Divide and Conquer
+    - Dynamic Programming
+    - Greedy Algorithms
+    - Backtracking
+ 
+
