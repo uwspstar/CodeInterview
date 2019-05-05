cde1d120ca9348dae53255c33cb6ff338a7dcb38
xing wang <uwspstar@gmail.com>
2019-05-05 17:41:42 -0500

    Update anagram.js

--- problem-solving-patterns/FREQUENCY-COUNTERS/anagram.js
+++ problem-solving-patterns/FREQUENCY-COUNTERS/anagram.js
@@ -42,3 +42,34 @@ console.log(validAnagram("rat","car")) // false) // false
 console.log(validAnagram('awesome', 'awesom')) // false
 console.log(validAnagram('qwerty', 'qeywrt'))// true
 console.log(validAnagram('texttwisttime', 'timetwisttext')) // true
+
+
+
+//if the space is main concern
+function validAnagram(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+
+  const lookup = {};
+
+  for (let i = 0; i < first.length; i++) {
+    let letter = first[i];
+    // if letter exists, increment, otherwise set to 1
+    lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+  }
+  console.log(lookup)
+
+  for (let i = 0; i < second.length; i++) {
+    let letter = second[i];
+    // can't find letter or letter is zero then it's not an anagram
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+
+  return true;
+}
+
