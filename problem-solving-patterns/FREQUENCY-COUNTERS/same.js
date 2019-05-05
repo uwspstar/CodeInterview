f14f9da13b6bcf96a1b1d1cbc8b636c573d7f0e0
xing wang <uwspstar@gmail.com>
2019-05-05 17:14:33 -0500

    Update same.js

--- problem-solving-patterns/FREQUENCY-COUNTERS/same.js
+++ problem-solving-patterns/FREQUENCY-COUNTERS/same.js
@@ -16,6 +16,82 @@ same([1,2,1], [4,4,1]) // false (must be same frequency)
 */
 
 
+- //O(n), use 3 loops instead of using a nest loop, use for...of (value) and for...in (key)
+const same3 = function(arr1, arr2){
+
+  if(arr1.length!=arr2.length)  return false;
+
+  let hashmap1 ={};
+  let hashmap2 ={};
+
+ //O(n)
+  for(let key of arr1){  
+    hashmap1[key]  =  (hashmap1[key] || 0) + 1;
+  }
+
+ //O(n)
+  for(let key of arr2){ 
+    hashmap2[key]  =  (hashmap2[key] || 0) + 1;
+  }   
+ 
+  //O(m)  and m<n
+  for(let key in hashmap1){  
+     
+      if( !hashmap2[key**2] || hashmap2[key ** 2] !== hashmap1[key]) {
+          return false;
+      }  
+  } 
+
+  return true;
+
+}
+
+console.log(same3([1,2,3], [4,1,9]));
+console.log(same3([1,2,3], [1,9]));
+console.log(same3([1,2,1], [4,4,1]));
+
+
+
+//O(n), use 3 loops instead of using a nest loop
+const same2 = function(arr1, arr2){
+
+  if(arr1.length!=arr2.length)  return false;
+
+  let hashmap1 ={};
+  let hashmap2 ={};
+
+ //O(n)
+  for(let i = 0; i <arr1.length; i++){ 
+    const key =arr1[i]
+    hashmap1[key]  =  (hashmap1[key] || 0) + 1;
+  }
+
+ //O(n)
+  for(let i = 0; i <arr2.length; i++){ 
+    const key =arr2[i]
+    hashmap2[key]  =  (hashmap2[key] || 0) + 1;
+  }  
+
+  const key1s  = Object.keys(hashmap1);
+  const key2s  = Object.keys(hashmap2);
+ 
+  //O(m)  and m<n
+  for(let i = 0; i< key1s.length; i++){   
+      if( key2s[i]!=(key1s[i]**2)  || hashmap1[key1s[i]] !=  hashmap2[key2s[i]]){
+          return false;
+      } 
+       
+  } 
+
+  return true;
+
+}
+
+console.log(same2([1,2,3], [4,1,9]));
+console.log(same2([1,2,3], [1,9]));
+console.log(same2([1,2,1], [4,4,1]));
+
+
 // Time Complexity - N^2
 const same = function(arr1, arr2){
 
