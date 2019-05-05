e8b02797dd159d7e70b2a567b5db41c030a5b5c9
xing wang <uwspstar@gmail.com>
2019-05-05 16:01:29 -0500

    Update same.js

--- problem-solving-patterns/FREQUENCY-COUNTERS/same.js
+++ problem-solving-patterns/FREQUENCY-COUNTERS/same.js
@@ -1,11 +1,10 @@
 
+
 /*
 FREQUENCY COUNTERS
 
 This pattern uses objects or sets to collect values/frequencies of values
-
 This can often avoid the need for nested loops or O(N^2) operations with arrays / strings
-
 
 Write a function called same, which accepts two arrays. 
 The function should return true if every value in the array has it's corresponding value squared in the second array. 
@@ -16,13 +15,28 @@ same([1,2,3], [1,9]) // false
 same([1,2,1], [4,4,1]) // false (must be same frequency)
 */
 
+
+// Time Complexity - N^2
 const same = function(arr1, arr2){
 
   if(arr1.length!=arr2.length)  return false;
 
+  for(let i = 0; i <arr1.length; i++){
 
+     
+    let index = arr2.indexOf(arr1[i] ** 2); //indexOf is o(n)
 
-  return false;
+    if(index ===-1) { 
+      return false;
+    }
+
+    console.log(arr2);
+
+    arr2.splice(index,1)
+  }
+
+  return true;
+
 }
 
 console.log(same([1,2,3], [4,1,9]));
