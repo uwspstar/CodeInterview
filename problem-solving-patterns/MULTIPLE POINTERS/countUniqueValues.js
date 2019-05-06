e926ea6f924c9954aca28a1bfe8a9f1cc54d40c5
xing wang <uwspstar@gmail.com>
2019-05-05 20:43:24 -0500

    Update countUniqueValues.js

--- problem-solving-patterns/MULTIPLE POINTERS/countUniqueValues.js
+++ problem-solving-patterns/MULTIPLE POINTERS/countUniqueValues.js
@@ -7,10 +7,28 @@ There can be negative numbers in the array,
 but it will always be sorted.
  */
 
+
+//use Set ( value is unique)
+var countUniqueValues1 =function(arr){ 
+  
+  //(new Set(arr)).length -- NOT WORKING, use Set.size ;
+  return (new Set(arr)).size;
+}
+
+console.log(countUniqueValues1([1,1,1,1,1,2])) // 2
+console.log(countUniqueValues1([1,2,3,4,4,4,7,7,12,12,13])) // 7
+console.log(countUniqueValues1([]))// 0
+console.log(countUniqueValues1([-1]))// 1
+console.log(countUniqueValues1([-2,-1,-1,0,1]))// 4
+
+
+
+//use hashmap
 var countUniqueValues =function(arr){
   
   if(arr.length < 2) return arr.length;
   let haspmap = {};
+  
   for(let val of arr){
     haspmap[val] = (haspmap[val] || 0) +1;
   }
