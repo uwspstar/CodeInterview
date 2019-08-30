98d01b5d787afc73bf217c3eb0151d5c48cc0dd0
xing wang <uwspstar@gmail.com>
2019-08-30 05:50:08 -0500

    Create Log-Number-Function.js

---
+++ The Coding Interview/Log-Number-Function.js
@@ -0,0 +1,22 @@
+Log Number Function
+
+var num = 50;
+ 
+function logNumber() {
+    console.log(num); 
+    var num = 100;
+}
+ 
+logNumber();
+
+//reason : var hoisting
+
+var num = 50;
+ 
+function logNumber() {
+    var num;
+    console.log(num); //Undefined
+    num = 100;
+}
+ 
+logNumber();
