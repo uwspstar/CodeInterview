e1491492ebc209b4f5cade9bf2809cbdd5ff5692
xing wang <uwspstar@gmail.com>
2019-08-29 21:25:43 -0500

    Update Hoisting.js

--- The Coding Interview/Hoisting.js
+++ The Coding Interview/Hoisting.js
@@ -113,3 +113,47 @@ be aware of.
 Constant Let are still both aware of the global scope and function scope.
 but constant let variables are also aware of another scope to call the block scope
 */
+
+
+// tab 1
+ 
+function getTotal() {
+  console.log(multiplier);
+  console.log(total);
+  
+  let total = 0;
+  
+  for(var i = 0; i < 10; i++) {
+    
+    let valueToAdd = i;
+    var multiplier = 2;
+    total += valueToAdd * multiplier;
+  }
+  
+  return total;
+}
+ 
+getTotal();
+ 
+ 
+ 
+// tab 2
+ 
+function getTotal() {
+  let total;
+  var multiplier;
+  
+  total = 0;
+  
+  for(var i = 0; i < 10; i++) {
+    let valueToAdd;
+    
+    valueToAdd = i;
+    multiplier = 2;
+    total += valueToAdd * multiplier;
+  }
+  
+  return total;
+}
+ 
+getTotal();
