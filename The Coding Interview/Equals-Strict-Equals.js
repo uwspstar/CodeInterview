a3cbd1167f177296ca4d3a7fa9226c88f4fc7b4c
xing wang <uwspstar@gmail.com>
2019-08-30 05:47:10 -0500

    Create Equals-Strict-Equals.js

---
+++ The Coding Interview/Equals-Strict-Equals.js
@@ -0,0 +1,12 @@
+
+
+31. Equals vs Strict Equals - Question
+"==" vs "==="
+
+"===" care about type and value
+"=="  care about value
+
+
+console.log(7 == '7');  // true
+ 
+console.log(7 === '7');  // false
