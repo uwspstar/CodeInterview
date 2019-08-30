78f786cac5e5cbd71c89466f11eb2bea25475493
xing wang <uwspstar@gmail.com>
2019-08-29 20:44:35 -0500

    Update Hoisting.js

--- The Coding Interview/Hoisting.js
+++ The Coding Interview/Hoisting.js
@@ -6,6 +6,18 @@ var color;  ( top of the file)
 
 color = blue
 ```
+```
+something happen for function expression
+
+var getTotal = function(){
+}
+
+
+will do " var getTotal;"  on the top
+
+then "getTotal = function (){...}
+
+```
 
 Hoisting - Answer Part 2 Source Code
 // tab 1
@@ -29,3 +41,17 @@ function getTotal() {
 }
  
 getTotal();
+
+
+
+/*
+difference between a function expression and a function declaration
+
+We can also declare a function without the VAR or Const keywords which is called a function declaration
+One important difference between a function expression and a function declaration is that they are hoisted
+differently for a function declaration. the whole function including its definition is hoisted to the top of the file.
+
+Using function declarations instead of function expressions can be very useful in your coding because
+with a function declaration the whole function is hoisted to the top of the file or to the top of the
+scope.
+*/
