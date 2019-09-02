fcbb697155d63217dbf3d131f1a4171bba7238bd
xing wang <uwspstar@gmail.com>
2019-09-01 21:55:22 -0500

    Create Array-Constructor.js

---
+++ The Coding Interview/Array-Constructor.js
@@ -0,0 +1,18 @@
+Array Constructor - Solution Source Code
+
+var arr1 = [];
+var arr2 = new Array(50);
+var arr3 = new Array(1, 2, "three", 4, "five");
+var arr4 = new Array([1, 2, 3, 4, 5]);
+ 
+console.log('arr1: ', arr1);
+console.log('arr2: ', arr2);
+console.log('arr3: ', arr3);
+console.log('arr4: ', arr4);
+
+/*
+arr1:  []
+arr2:  [ <50 empty items> ]
+arr3:  [ 1, 2, 'three', 4, 'five' ]
+arr4:  [ [ 1, 2, 3, 4, 5 ] ]
+*/
