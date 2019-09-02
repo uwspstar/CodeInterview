43d8625760f886a5069a1969730b911dcb80dcb9
xing wang <uwspstar@gmail.com>
2019-09-01 19:36:08 -0500

    Create Order-Logged-Out.js

---
+++ The Coding Interview/Order-Logged-Out.js
@@ -0,0 +1,18 @@
+//Order Logged Out - Solution Source Code
+
+function logNumbers() {
+  console.log(1); 
+  setTimeout(function(){console.log(2)}, 1000); 
+  setTimeout(function(){console.log(3)}, 0); 
+  console.log(4);
+}
+ 
+logNumbers();
+
+/*
+1
+4
+3
+2
+*/
+   
