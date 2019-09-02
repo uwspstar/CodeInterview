e419473450b7f5e6847dc6acd8282b6da1c0e72d
xing wang <uwspstar@gmail.com>
2019-09-01 22:11:58 -0500

    Create Equivalent-Numbers.js

---
+++ The Coding Interview/Equivalent-Numbers.js
@@ -0,0 +1,16 @@
+//Equivalent Numbers - Solution Source Code
+
+
+console.log(900.9 === 300.3 * 3);
+console.log(300.3 * 3);
+console.log(Number((300.3 * 3).toFixed(2)));
+console.log(Number((300.3 * 3).toPrecision(12)));
+console.log(((300.3 * 10) * 3) / 10);
+
+/*
+false
+900.9000000000001
+900.9
+900.9
+900.9
+*/
