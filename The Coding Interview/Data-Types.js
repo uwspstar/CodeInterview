ba43713ee1595e23136038aba5446b92341ca6ff
xing wang <uwspstar@gmail.com>
2019-09-01 21:11:12 -0500

    Create Data-Types.js

---
+++ The Coding Interview/Data-Types.js
@@ -0,0 +1,17 @@
+Type of Data Types - Solution Source Code
+
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof {});
+console.log(typeof []);
+console.log(Array.isArray([]));
+console.log([] instanceof Array);
+
+/*
+object
+undefined
+object
+object
+true
+true
+*/
