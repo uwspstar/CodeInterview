94c904b6cdd0f215c1d6ee120aa8605dcf5af1cd
xing wang <uwspstar@gmail.com>
2019-09-01 22:24:33 -0500

    Update and rename Strings-and-Arrays to Strings-and-Arrays.js

--- The Coding Interview/Strings-and-Arrays.js
+++ The Coding Interview/Strings-and-Arrays.js
@@ -1,6 +1,9 @@
 
 
 // Strings and Arrays - Question Source Code
+// only use 'read-only' methods: filter, forEach, map, some, every, etc.
+// cannot use: push, pop, splice, shift, reverse, etc.  change the string
+// [].filter same as Array.protype.filter...
 
 const data1 = 'Jordan Smith';
  
@@ -9,3 +12,6 @@ const data2 = [].filter.call(data1, function(elem, index) {
 });
  
 console.log(data2); //[ 'S', 'm', 'i', 't', 'h' ]
+
+
+
