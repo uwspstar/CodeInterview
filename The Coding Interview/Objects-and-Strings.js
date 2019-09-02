1e26ce8d5bc97d011740dfc37c1d8465e5466267
xing wang <uwspstar@gmail.com>
2019-09-01 22:15:23 -0500

    Create Objects-and-Strings.js

---
+++ The Coding Interview/Objects-and-Strings.js
@@ -0,0 +1,20 @@
+//Objects and Strings - Solution Source Code
+
+
+var string1 = 'Tampa';
+var string2 = string1;
+string1 = 'Venice';
+ 
+console.log(string2); // Tampa
+ 
+ 
+var person1 = {
+  name: 'Alex',
+  age: 30
+};
+ 
+var person2 = person1;
+ 
+person2.name = 'Kyle';
+ 
+console.log(person1); // { name: 'Kyle', age: 30 }
