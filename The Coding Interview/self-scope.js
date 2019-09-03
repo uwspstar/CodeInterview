06f3e40f92e14aa51c7588113714d56a7f548e6b
xing wang <uwspstar@gmail.com>
2019-09-02 21:26:54 -0500

    Update self-scope.js

--- The Coding Interview/self-scope.js
+++ The Coding Interview/self-scope.js
@@ -1,8 +1,8 @@
 
 
-//Scope and "self"
-//function is an object
-//"this" for the object owner (a function object ower), js word, the gloale obj is windows
+// Scope and "self"
+// function is an object
+// "this" for the object owner (a function object ower), js word, the gloale obj is windows
 
 
 var myCar = {
@@ -11,12 +11,21 @@ var myCar = {
         var self = this;
         console.log("In logColor - this.color: " + this.color); // "this" refers to myCar obj
         console.log("In logColor - self.color: " + self.color); // "self" equel this refer to myCar object
+        
         (function() {
             console.log("In IIFE - this.color: " + this.color); 
             // "this" lost refers to myCar, will point globle object which we do not have
             console.log("In IIFE - self.color: " + self.color); // "this refers to myCar
         })();
+        
     }
 };
  
 myCar.logColor();
+
+/*
+In logColor - this.color: Blue
+In logColor - self.color: Blue
+In IIFE - this.color: undefined
+In IIFE - self.color: Blue
+*/
