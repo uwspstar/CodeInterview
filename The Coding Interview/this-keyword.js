ecf1086d727b0188aa10da97bf7bc70c5b17d7c8
xing wang <uwspstar@gmail.com>
2019-09-02 21:22:29 -0500

    Update this-keyword.js

--- The Coding Interview/this-keyword.js
+++ The Coding Interview/this-keyword.js
@@ -3,20 +3,25 @@
 It has different values depending on where it is used:
 
 In a method, this refers to the owner object.
+
 Alone, this refers to the global object.
+
 In a function, this refers to the global object.
 In a function, in strict mode, this is undefined.
 In an event, this refers to the element that received the event.
+
 Methods like call(), and apply() can refer this to any object.
+
 ```
 
-"this" Keyword - Solution Source Code
+//"this" Keyword - Solution Source Code
+
 var house = {
   price: 100000,
   squareFeet: 2000,
   owner: 'Taylor',
   getPricePerSquareFoot: function() {
-    return this.price / this.squareFeet;
+    return this.price / this.squareFeet; //refer house obj
   }
 };
  
