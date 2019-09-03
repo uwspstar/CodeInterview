0787a86a25926e3b477477d9125cbfebf28a3e2a
xing wang <uwspstar@gmail.com>
2019-09-02 21:19:00 -0500

    Update Hoisting.js

--- The Coding Interview/Hoisting.js
+++ The Coding Interview/Hoisting.js
@@ -1,11 +1,53 @@
+function bigBrother(){
+  function littleBrother() {
+    return 'it is me!';
+  }
+  
+  return littleBrother();
+  
+  function littleBrother() {
+    return 'no me!';
+  }
+}
+
+// Before running this code, what do you think the output is?
+console.log(bigBrother()); 
+
+
 ```
+
+function bigBrother(){
+  function littleBrother() {
+    return 'it is me!';
+  }
+  
+  function littleBrother() { //over write first one
+    return 'no me!';
+  }
+  return littleBrother();
+  
+  
+}
+
+// Before running this code, what do you think the output is?
+console.log(bigBrother()); //'no me!'
+
+```
+
+
 var color = blue
 
+```
 infact in javascript, declar on top, assign value later, it is Hoisting
+
 var color;  ( top of the file)
 
 color = blue
 ```
+
+var getTotal = function(){
+}
+
 ```
 something happen for function expression
 
@@ -15,7 +57,7 @@ var getTotal = function(){
 
 will do " var getTotal;"  on the top
 
-then "getTotal = function (){...}
+then getTotal = function (){...}
 
 ```
 ```
@@ -35,7 +77,7 @@ console.log(color); // blue
  
  
  
-//tab 2 .( reason)
+//tab 2 .(reason)
  
 var color;
  
@@ -93,25 +135,31 @@ var globalVar = 'global';
 difference between a function expression and a function declaration
 
 We can also declare a function without the VAR or Const keywords which is called a function declaration
+
 One important difference between a function expression and a function declaration is that they are hoisted
-differently for a function declaration. the whole function including its definition is hoisted to the top of the file.
+differently 
+
+for a function declaration. 
+the whole function including its definition is hoisted to the top of the file.
 
 Using function declarations instead of function expressions can be very useful in your coding because
 with a function declaration the whole function is hoisted to the top of the file or to the top of the
 scope.
+
 */
 
+// function scope vs block scope
 /*
 Var is function scope not block scope
 
 In reality this is only true when we are using the VAR keyword to declare a variable because the VAR
 keyword is function scoped.
 
-If however we use the const or let keywords to declare a variable there is another scope that we must
-be aware of.
+If however we use the const or let keywords to declare a variable
+there is another scope that we must be aware of.
 
 Constant Let are still both aware of the global scope and function scope.
-but constant let variables are also aware of another scope to call the block scope
+but constant, an let variables are also aware of another scope to call the block scope
 */
 
 
@@ -143,6 +191,9 @@ function getTotal() {
   let total;
   var multiplier;
   
+  console.log(multiplier); // Undefined
+  console.log(total); // Undefined
+ 
   total = 0;
   
   for(var i = 0; i < 10; i++) {
@@ -150,6 +201,7 @@ function getTotal() {
     
     valueToAdd = i;
     multiplier = 2;
+   
     total += valueToAdd * multiplier;
   }
   
