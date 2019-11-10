35c84aecdf4d84851b96155bff66d5d4b00bcd72
xing wang <uwspstar@gmail.com>
2019-11-10 05:30:39 -0600

    Create call vs apply vs bind.js

---
+++ review/call vs apply vs bind.js
@@ -0,0 +1,22 @@
+const wizard = {
+  name: 'Merlin',
+  health: 100,
+  heal: function(num1, num2) {
+    this.health += num1 + num2;
+  }
+}
+
+const archer = {
+  name: 'Robin Hood',
+  health: 50
+}
+
+wizard.heal.call(archer, 50, 60)
+wizard.heal.apply(archer, [20, 30])
+archer
+// function borrowing
+
+const healArcher = wizard.heal.bind(archer, 50, 60);
+console.log(archer)
+healArcher()
+console.log(archer)
