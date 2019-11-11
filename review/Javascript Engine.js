b1bf11bef668d73851fb3666d7934ba599ef172c
xing wang <uwspstar@gmail.com>
2019-11-11 05:26:21 -0600

    Update Javascript Engine.js

--- review/Javascript Engine.js
+++ review/Javascript Engine.js
@@ -2,6 +2,9 @@
 
 // there aer more Javascript Engine
 
-// Interpreters and Compilers
+// Interpreters and Compilers 
+// (runtime vs compiler time)
 
 // 2 ways to convert js code to machine code
+// Interpreters excute code line by line
+// Compilers the code to machine understand code before run
