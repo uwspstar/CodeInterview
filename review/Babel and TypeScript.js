bdb393ae1c088a8531b579d4cf36206b4c4e0f5f
xing wang <uwspstar@gmail.com>
2019-11-11 05:28:50 -0600

    Create Babel and TypeScript.js

---
+++ review/Babel and TypeScript.js
@@ -0,0 +1,4 @@
+// Babel is a Javascript compiler that takes your modern JS code and returns  browser compatible JS (older JS code).
+// Typescript is a superset of Javascript that compiles down to Javascript.
+
+// Both of these do exactly what compilers do: Take one language and convert into a different one!
