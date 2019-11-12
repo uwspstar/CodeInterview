4de313c5017c9c8bdbc4ae705329a84d4501ae2a
xing wang <uwspstar@gmail.com>
2019-11-12 03:52:26 -0600

    Update Javascript Engine.js

--- review/Javascript Engine.js
+++ review/Javascript Engine.js
@@ -8,3 +8,5 @@
 // 2 ways to convert js code to machine code
 // Interpreters excute code line by line
 // Compilers the code to machine understand code before run
+
+// JIT Compilers (browser use it)
