28812dc7eed4b55457fcf2ae5cbf6287852015ea
xing wang <uwspstar@gmail.com>
2019-11-14 05:13:52 -0600

    Update Strings-and-Arrays.js

--- The Coding Interview/Strings-and-Arrays.js
+++ The Coding Interview/Strings-and-Arrays.js
@@ -11,7 +11,9 @@ const data2 = [].filter.call(data1, function(elem, index) {
   return index > 6;
 });
  
-console.log(data2); //[ 'S', 'm', 'i', 't', 'h' ]
+console.log(data2); // [ 'S', 'm', 'i', 't', 'h' ]
+
+// use call method to use as "this"
 
 
 
